a131934e9701b393566c159f2b8b34a5b8f86216
mihail23 <mihailspak23@gmail.com>
2026-07-14 21:58:53 +1000

    новые задания

--- ege/program_171.py
+++
@@ -1,8 +0,0 @@
-l = input().split('-')
-flag = 'NO'
-if len(l) == 4 and len(l[0]) == 1 and l[0] == '7' and len(l[1]) == 3 and len(l[2]) == 3 and len(l[3]) == 4 and ''.join(l).isdigit():
-    flag =  'YES'
-if len(l) == 3 and len(l[0]) == 3 and len(l[1]) == 3 and len(l[2]) == 4 and ''.join(l).isdigit():
-    flag = 'YES'
-print(flag)
-
